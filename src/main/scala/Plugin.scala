3a43abd9ebbf58acabc8fb57125c5c239b072a6c
Matthieu Brouillard <matthieu@brouillard.fr>
2015-08-05 11:20:26 +0200

    initial creation of gitbucket-announce-plugin

---
+++ src/main/scala/Plugin.scala
@@ -0,0 +1,39 @@
+import javax.servlet.ServletContext
+
+import fr.brouillard.gitbucket.announce.controller.AnnounceController
+import gitbucket.core.plugin.PluginRegistry
+import gitbucket.core.service.SystemSettingsService.SystemSettings
+import gitbucket.core.util.Version
+
+class Plugin extends gitbucket.core.plugin.Plugin {
+  override val pluginId: String = "announce"
+
+  override val pluginName: String = "Announce Plugin"
+
+  override val description: String = "Allows to handle announces for gitbucket"
+
+  override val versions: List[Version] = List(
+    Version(1, 0)
+  )
+
+  override def javaScripts(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Seq[(String, String)] = {
+    // Add Snippet link to the header
+    val path = settings.baseUrl.getOrElse(context.getContextPath)
+    Seq(
+      ".*/admin/announce" -> s"""
+        |$$('#system-admin-menu-container>li:last').after(
+        |  $$('<li class="active"><a href="${path}/admin/announce">Global Announce</a></li>')
+        |);
+      """.stripMargin,
+      ".*/admin/(?!announce).*" -> s"""
+        |$$('#system-admin-menu-container>li:last').after(
+        |  $$('<li><a href="${path}/admin/announce">Global Announce</a></li>')
+        |);
+      """.stripMargin
+    )
+  }
+
+  override val controllers = Seq(
+    "/admin/announce" -> new AnnounceController()
+  )
+}
---
+++ src/main/scala/fr/brouillard/gitbucket/announce/controller/AnnounceController.scala
@@ -0,0 +1,74 @@
+package fr.brouillard.gitbucket.announce.controller
+
+import fr.brouillard.gitbucket.announce.html
+import gitbucket.core.controller.ControllerBase
+import gitbucket.core.service.{AccountService, SystemSettingsService}
+import gitbucket.core.servlet.Database
+import gitbucket.core.util.AdminAuthenticator
+import jp.sf.amateras.scalatra.forms._
+import org.apache.commons.mail.{DefaultAuthenticator, HtmlEmail}
+import org.pegdown.PegDownProcessor
+import org.slf4j.LoggerFactory
+
+class AnnounceController extends AnnounceControllerBase
+with AdminAuthenticator
+
+trait AnnounceControllerBase extends ControllerBase with AccountService {
+  self:  AdminAuthenticator =>
+
+  private val logger = LoggerFactory.getLogger(classOf[AnnounceController])
+
+  case class AnnounceForm(content: String, subject: String)
+
+  private val announceForm = mapping(
+    "content" -> trim(label("Announce", text(required))),
+    "subject" -> trim(label("Subject", text(required)))
+  )(AnnounceForm.apply)
+
+  get("/admin/announce")(adminOnly {
+    html.announce(flash.get("info"))
+  })
+
+  post("/admin/announce", announceForm)(adminOnly { form =>
+    flash += "info" -> "Announce has been sent."
+
+    if (logger.isDebugEnabled) {
+      logger.debug("sending announce: {}", form.content)
+    }
+
+    val systemSettings = new SystemSettingsService {}.loadSystemSettings
+    if (systemSettings.notification && systemSettings.smtp.nonEmpty) {
+      val email = new HtmlEmail
+      val smtp = systemSettings.smtp.get
+
+      email.setHostName(smtp.host)
+      email.setSmtpPort(smtp.port.get)
+      smtp.user.foreach { user =>
+        email.setAuthenticator(new DefaultAuthenticator(user, smtp.password.getOrElse("")))
+      }
+      smtp.ssl.foreach { ssl =>
+        email.setSSLOnConnect(ssl)
+      }
+      smtp.fromAddress
+        .map (_ -> smtp.fromName.orNull)
+        .orElse (Some("notifications@gitbucket.com" -> context.loginAccount.get.userName))
+        .foreach { case (address, name) =>
+        email.setFrom(address, name)
+      }
+      email.setCharset("UTF-8")
+      email.setSubject(form.subject)
+
+      email.setHtmlMsg(new PegDownProcessor().markdownToHtml(form.content))
+
+      logger.info("sending email: {}", form.content)
+      val database = Database()
+      database withSession { implicit session =>
+        getAllUsers(false).filter(account => !account.isGroupAccount && account.mailAddress.nonEmpty).foreach(account => email.addBcc(account.mailAddress))
+      }
+
+      email.send()
+    }
+
+    redirect("/admin/announce")
+  })
+}
