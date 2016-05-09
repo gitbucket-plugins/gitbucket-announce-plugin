776e72c5a515338b5ec74d30cde816d76fb8fd5d
Matthieu Brouillard <matthieu@brouillard.fr>
2016-05-09 17:23:03 +0200

    update to gitbucket 4.0.0; fixes #11

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,9 +1,7 @@
-import javax.servlet.ServletContext
-
 import fr.brouillard.gitbucket.announce.controller.AnnounceController
-import gitbucket.core.plugin.PluginRegistry
-import gitbucket.core.service.SystemSettingsService.SystemSettings
-import gitbucket.core.util.Version
+import gitbucket.core.controller.Context
+import gitbucket.core.plugin.{Link, PluginRegistry}
+import io.github.gitbucket.solidbase.model.Version
 
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "announce"
@@ -13,28 +11,16 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val description: String = "Allows to handle announces for gitbucket"
 
   override val versions: List[Version] = List(
-    Version(1, 3)
-    , Version(1, 2)
-    , Version(1, 1)
-    , Version(1, 0)
+    new Version("1.4.0")
+    , new Version("1.3.0")
+    , new Version("1.2.0")
+    , new Version("1.1.0")
+    , new Version("1.0.0")
   )
 
-  override def javaScripts(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Seq[(String, String)] = {
-    // Add Snippet link to the header
-    val path = settings.baseUrl.getOrElse(context.getContextPath)
-    Seq(
-      ".*/admin/announce" -> s"""
-        |$$('#system-admin-menu-container>li:last').after(
-        |  $$('<li class="active"><a href="${path}/admin/announce">Global Announce</a></li>')
-        |);
-      """.stripMargin,
-      ".*/admin/(?!announce).*" -> s"""
-        |$$('#system-admin-menu-container>li:last').after(
-        |  $$('<li><a href="${path}/admin/announce">Global Announce</a></li>')
-        |);
-      """.stripMargin
-    )
-  }
+  override val systemSettingMenus: Seq[(Context) => Option[Link]] = Seq(
+    (ctx: Context) => Some(Link("announce", "Global Announce", "admin/announce"))
+  )
 
   override val controllers = Seq(
     "/admin/announce" -> new AnnounceController()
